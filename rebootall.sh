29f1665c0f6f023a451e8a05f8358ea7dfc083e5
Devin Mitchell <dpmitche@Devins-MacBook-Pro.local>
2023-10-06 12:59:19 +0100

    DPM-20231006 Updating for new sensit oil tank sensor

--- rebootall.sh
+++ rebootall.sh
@@ -13,4 +13,6 @@ curl "http://192.168.0.15/reboot?" &
 curl "http://192.168.0.139/reboot?" &
 curl "http://192.168.0.31/reboot?" &
 curl "http://192.168.0.113/reboot?" &
+
 curl "http://192.168.0.124/reboot?" &
+curl "http://192.168.0.116/reboot?" &
